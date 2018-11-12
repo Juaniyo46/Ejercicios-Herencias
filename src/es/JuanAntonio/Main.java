67dac5a4cdb734deb230eb12b88a9111282d822d
Juan <juan.alvarez@escuelaestech.es>
2018-11-12 11:13:58 +0100

    Ejercicio clase sueldo y programadores (1)

--- src/es/JuanAntonio/Main.java
+++ src/es/JuanAntonio/Main.java
@@ -3,8 +3,19 @@ package es.JuanAntonio;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Square cuadrado = new Square( "Amarillo");
-        cuadrado.getSides();
+
+        Programador programador1 = new Programador("Dergachi","PHP");
+        Programador programador2 = new Programador("Dergachi", "Java");
+        Analista analista1 = new Analista("Dergachi", "Arquitectura de software");
+
+        programador1.setSueldo(25.5);
+
+        programador2.setSueldo(37.6);
+
+        analista1.setSueldo(55);
+
+        System.out.println(programador1);
+        System.out.println(programador2);
+        System.out.println(analista1);
     }
 }
